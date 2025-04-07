7f2f0146dd0a0bf14ecc522e9bb9d48808e824dd
Shri9345 <shridharpatilsp111@gmail.com>
2025-04-07 13:20:54 +0530

    index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${443}`);
 });
